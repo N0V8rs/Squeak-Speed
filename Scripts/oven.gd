fe3d7143475b330d8b751cfeae2c22d9709ebd40
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2024-05-03 00:52:43 -0300

    Oven Trap Done
    Needs testing for hitboxes for the Player and make sure that the oven is drawn behind the Player

---
+++ Scripts/oven.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var oven_off_sprite = $OvenOffSprite
+@onready var oven_warn_sprite = $OvenWarnSprite
+@onready var oven_hot_sprite = $OvenHotSprite
+@onready var oven_timer = $OvenTimer
+
+var current_oven_sprite = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	oven_off_sprite.visible = true
+	oven_timer.start()
+	print("OFF Oven spawned")
+
+func _on_oven_timer_timeout():
+	if current_oven_sprite == 1:
+		oven_off_sprite.visible = false
+		oven_warn_sprite.visible = true
+		current_oven_sprite = 2
+	elif current_oven_sprite == 2:
+		oven_warn_sprite.visible = false
+		oven_hot_sprite.visible = true
+		current_oven_sprite = 3
+	elif current_oven_sprite == 3:
+		oven_hot_sprite.visible = false
+		oven_warn_sprite.visible = true
+		current_oven_sprite = 4
+	elif current_oven_sprite == 4:
+		oven_warn_sprite.visible = false
+		oven_off_sprite.visible = true
+		current_oven_sprite = 1
+	oven_timer.start()
