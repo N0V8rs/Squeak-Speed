fda085daa35890a635718c338b105589b1c29810
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2024-05-07 23:05:17 -0300

    Added the timer for Checkpoints
    Bug it will add another Timer to the already timer

--- Scripts/CheckpointBox.gd
+++ Scripts/CheckpointBox.gd
@@ -1,9 +1,26 @@
 extends Area2D
 
+# Move this clock to the Player scene
+# It will add another timer when entering the checkpoint
+var current_timer = null
+@export var timer_time = 10
+@onready var clock_timer = $"../Clock Timer"
+
 # Checkpoint animation if there is one goes in this func
 func _ready():
-	pass # Replace with function body.
+	clock_timer.start(timer_time)
+	pass
+	
+func _process(delta):
+	print(clock_timer.time_left)
+	pass
 
 func _on_body_entered(body):
+	if body.name == "Player":
+		clock_timer.stop()
+		clock_timer.start(timer_time)
 	Checkpoint.player_last_position = global_position
 	print("Checkpoint Reached")
+
+func _on_clock_timer_timeout():
+	get_tree().reload_current_scene()
