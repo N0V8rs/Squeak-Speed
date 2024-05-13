9f5158dd6e5e993f2f41663d899e97540e4ce158
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2024-05-12 22:19:00 -0300

    Oven Trap
    Actually works works not like how Unity does gameobjects, changed the knife animations too for some of them have hitboxes in the wrong frame

--- Scripts/CheckpointManager.gd
+++ Scripts/CheckpointManager.gd
@@ -11,7 +11,8 @@ var can_restart_timer:bool = true
 signal second_changed
 
 func _ready():
-	get_tree().change_scene_to_packed.call_deferred(MAIN_MENU)
+	#get_tree().change_scene_to_packed.call_deferred(MAIN_MENU)
+	pass
 
 func fade_out_in():
 	var tween = fader.create_tween()
--- Scripts/oven.gd
+++ Scripts/oven.gd
@@ -7,33 +7,44 @@ extends Node2D
 
 var current_oven_sprite = 1
 
-# IMPORTANT
-# Actually doesn't work with hitboxes
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	oven_off_sprite.visible = true
-	oven_off_sprite.is_processing()
-	oven_warn_sprite.hide()
-	oven_hot_sprite.hide() 
-	#oven_timer.start()
+	oven_warn_sprite.visible = false
+	oven_hot_sprite.visible = false
+	oven_off_sprite.set_process(true)
+	oven_warn_sprite.set_process(false)
+	oven_hot_sprite.set_process(false)
+	oven_timer.start()
 	print("OFF Oven spawned")
 
 func _on_oven_timer_timeout():
 	if current_oven_sprite == 1:
-		oven_off_sprite.is_processing()
-		#oven_warn_sprite.set_process()
+		oven_off_sprite.set_process(false)
+		oven_off_sprite.visible = false
+		oven_warn_sprite.set_process(true)
+		oven_warn_sprite.visible = true
 		current_oven_sprite = 2
+		print("Oven warm")
 	elif current_oven_sprite == 2:
+		oven_warn_sprite.set_process(false)
 		oven_warn_sprite.visible = false
+		oven_hot_sprite.set_process(true)
 		oven_hot_sprite.visible = true
 		current_oven_sprite = 3
+		print("Oven Hot")
 	elif current_oven_sprite == 3:
+		oven_hot_sprite.set_process(false)
 		oven_hot_sprite.visible = false
+		oven_warn_sprite.set_process(true)
 		oven_warn_sprite.visible = true
 		current_oven_sprite = 4
+		print("Oven Warm")
 	elif current_oven_sprite == 4:
+		oven_warn_sprite.set_process(false)
 		oven_warn_sprite.visible = false
+		oven_off_sprite.set_process(true)
 		oven_off_sprite.visible = true
 		current_oven_sprite = 1
+		print("Oven off")
 	oven_timer.start()
