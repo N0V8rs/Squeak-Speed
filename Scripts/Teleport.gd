7653b893167b8b0d9005f7ceaf42f65bfb5ee0a4
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2024-05-21 16:59:47 -0300

    MORE LEVELS!!! and minor fixes
    Levels 4 - 6 have been added and 6 still needs to be polished, steam for the oven and hot sauce have been updated

--- Scripts/Teleport.gd
+++ Scripts/Teleport.gd
@@ -1,8 +1,8 @@
 extends Node
 
-@onready var teleport = $Position2D
 @onready var player = %Player
+@onready var position_2d = $"../../../Level 5/Traps/Teleport/Position2D"
 
 func _on_area_2d_body_entered(body):
 	if body is Player:
-		player.position = teleport.get_global_transform().origin
+		player.position = position_2d.get_global_transform().origin
